bdc78bac6140683e6c4fcabc619974238e9c5901
Shannon Kuehn <skuehn@Shannons-MacBook-Pro.local>
2018-11-01 23:02:55 -0500

    Finalizing files for delivery.

---
+++ 10.12.18/PowerShell/PoShell-NetCreate.ps1
@@ -0,0 +1,63 @@
+﻿#Logs into an Azure subscription
+Login-AzureRmAccount
+
+#Selects the Azure subscription
+Select-AzureRmSubscription -Subscription "<insert-subscriptionId-guid-here>"
+
+#Creates a new Azure ResourceGroup
+New-AzureRmResourceGroup -Name ajgPoSh -Location EastUS
+
+#Creates virtual network
+$virtualNetwork = New-AzureRmVirtualNetwork `
+  -ResourceGroupName ajgPoSh `
+  -Location EastUS `
+  -Name poshVnet `
+  -AddressPrefix 10.0.0.0/16
+
+#Creates a subnet configuration within virtual network
+$subnetConfig = Add-AzureRmVirtualNetworkSubnetConfig `
+  -Name default `
+  -AddressPrefix 10.0.0.0/24 `
+  -VirtualNetwork $virtualNetwork
+
+#Creates a subnet  within the virtual network
+$virtualNetwork | Set-AzureRmVirtualNetwork
+
+#Creates VM1  in Azure
+New-AzureRmVm `
+    -ResourceGroupName "ajgPoSh" `
+    -Location "East US" `
+    -VirtualNetworkName "poshVnet" `
+    -SubnetName "subnet1" `
+    -Name "Vm1" `
+    -AsJob
+
+#Creates VM2 in Azure
+New-AzureRmVm `
+  -ResourceGroupName "ajgPoSh" `
+  -VirtualNetworkName "poshVnet" `
+  -SubnetName "subnet1" `
+  -Name "Vm2"
+  -AsJob
+
+#Gets IP Address of VM1
+Get-AzureRmPublicIpAddress `
+  -Name myVm1 `
+  -ResourceGroupName ajgPoSh `
+  | Select IpAddress
+
+#Gets IP Address of VM2
+Get-AzureRmPublicIpAddress `
+  -Name myVm2 `
+  -ResourceGroupName ajgPoSh `
+  | Select IpAddress
+
+#Initiates RDP prompt to connect to Windows VM
+mstsc /v:137.117.68.64
+mstsc /v:23.96.86.249
+
+#Run against VM2
+New-NetFirewallRule –DisplayName “Allow ICMPv4-In” –Protocol ICMPv4
+
+#Removes Resource Group
+Remove-AzureRmResourceGroup -Name ajgPoSh -Force
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# NetworkTraining
+# Introduction 
+Networking on Azure training - prepared for Arthur J. Gallagher - 2018
+
+# Schedule
+Session Breakdown
+08.10.2018
+1.	Establishing Connectivity in Azure
+2.	Load Balancers, Traffic Manager, and Application Gateways
+3.	Traffic Routing and Internet Security
+
+10.12.2018
+1.	Service Endpoints
+2.	Building a network with PowerShell
+3.	Building a network with the Azure CLI
+1.	Building a network with Azure Resource Manager (ARM) Templates
+
